26411c85369fb8b0b844bd3b03a8713b17c4b429
Minimalon <grafxartyr@gmail.com>
2021-11-07 18:16:35 +0300

    delete TestTest

--- TestTest/a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "1"
-echo "1" >> /root/TestTest/c.txt
-n=`tail -n10 /root/TestTest/c.txt | grep -c '1'`
-
-if [ $n == 1]; then
-echo "3"
-fi
--- TestTest/b.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /root/TestTest
-. a.sh
