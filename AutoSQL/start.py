c3a9c780b27c4ab547ac42f22b54802bb5f9d530
Minimalon <grafxartyr@gmail.com>
2021-12-21 19:21:19 +0300

    .

--- AutoSQL/start.py
+++ AutoSQL/start.py
@@ -8,7 +8,7 @@ from pymysql.cursors import DictCursor
 
 ttnload = list(reversed(os.listdir("/root/ttnload/TTN/")))
 for i in ttnload:
-    if i != "TTN":
+    if "TTN" not in i:
         print("Remove ",i)
         ttnload.remove(i)
 
@@ -21,8 +21,8 @@ with closing(pymysql.connect(host='localhost', user='root', password='', db='dic
                     with open("/root/ttnload/TTN/" + TTN + "/WayBill_v4.xml") as f:
                         for line in f:
                             line = line.replace("<","\n<")
-                            if "<pref:ShortName>" in line:
-                                ShortName = re.split(">|\n<", line)[2]
+                            if "<pref:FullName>" in line:
+                                FullName = re.split(">|\n<", line)[2]
                             if "<pref:Capacity>" in line:
                                 Capacity = re.split(">|\n<", line)[2]
                             if "<wb:Price>" in line:
@@ -30,8 +30,8 @@ with closing(pymysql.connect(host='localhost', user='root', password='', db='dic
                             if "<wb:EAN13>" in line:
                                 EAN = re.split(">|\n<", line)[2]
 
-                                insert1 = "INSERT IGNORE INTO tmc (bcode, vatcode1, vatcode2, vatcode3, vatcode4, vatcode5, dcode, name, articul, cquant, measure, pricetype, price, minprice, valcode, quantdefault, quantlimit, ostat, links, quant_mode, bcode_mode, op_mode, dept_mode, price_mode, tara_flag, tara_mode, tara_default, unit_weight, code, aspectschemecode, aspectvaluesetcode, aspectusecase, aspectselectionrule, extendetoptions, groupcode, remain, remaindate, documentquantlimit, age, alcoholpercent, inn, kpp, alctypecode, manufacturercountrycode, paymentobject, loyaltymode, minretailprice) VALUES ('"+ EAN +"',301,302,303,304,305,1,'"+ ShortName + " " + Capacity + "л""','',1.000,2114,0,0.00,"+ price.split(".")[0] +".00,0,1.000,0.000,0,0,15,3,192,1,1,NULL,NULL,'0',NULL,'"+ ShortName + " " + Capacity + "л""',NULL,NULL,NULL,NULL,NULL,NULL,0.000,NULL,2.000,NULL,15.00,NULL,NULL,0,NULL,NULL,0,0.00)"
-                                insert2 = "INSERT IGNORE INTO barcodes (code, barcode, name, price, cquant, measure, aspectvaluesetcode, quantdefault, packingmeasure, packingprice, minprice, minretailprice, customsdeclarationnumber, tmctype) VALUES ('" + EAN + "','" + EAN + "','" + ShortName + " " + Capacity + "л""'," + price.split(".")[0] + ",NULL,2,NULL,1.000,2,NULL,0.00,NULL,NULL,NULL)"
+                                insert1 = "INSERT IGNORE INTO tmc (bcode, vatcode1, vatcode2, vatcode3, vatcode4, vatcode5, dcode, name, articul, cquant, measure, pricetype, price, minprice, valcode, quantdefault, quantlimit, ostat, links, quant_mode, bcode_mode, op_mode, dept_mode, price_mode, tara_flag, tara_mode, tara_default, unit_weight, code, aspectschemecode, aspectvaluesetcode, aspectusecase, aspectselectionrule, extendetoptions, groupcode, remain, remaindate, documentquantlimit, age, alcoholpercent, inn, kpp, alctypecode, manufacturercountrycode, paymentobject, loyaltymode, minretailprice) VALUES ('"+ EAN +"',301,302,303,304,305,1,'"+ FullName + " " + Capacity + "л""','',1.000,2114,0,0.00,"+ price.split(".")[0] +".00,0,1.000,0.000,0,0,15,3,192,1,1,NULL,NULL,'0',NULL,'"+ FullName + " " + Capacity + "л""',NULL,NULL,NULL,NULL,NULL,NULL,0.000,NULL,2.000,NULL,15.00,NULL,NULL,0,NULL,NULL,0,0.00)"
+                                insert2 = "INSERT IGNORE INTO barcodes (code, barcode, name, price, cquant, measure, aspectvaluesetcode, quantdefault, packingmeasure, packingprice, minprice, minretailprice, customsdeclarationnumber, tmctype) VALUES ('" + EAN + "','" + EAN + "','" + FullName + " " + Capacity + "л""'," + price.split(".")[0] + ",NULL,2,NULL,1.000,2,NULL,0.00,NULL,NULL,NULL)"
 
                                 cursor.execute(insert1)
                                 cursor.execute(insert2)
