37c352b234e224082a02369d9ebbba53d630b125
Minimalon <grafxartyr@gmail.com>
2022-01-18 20:31:41 +0300

    delete ArturPuppet

--- ArturPuppet/linuxcash-cash-conf-autostart/lock_caps.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sleep 5
-DISPLAY=:255 XAUTHORITY=/home/autologon/.Xauthority sudo -u autologon xmodmap -e "keycode 66 = NoSymbol"
--- ArturPuppet/root/AutoSQL/cron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt install python3.4 -y
-cd /root/AutoSQL
-./start.py
